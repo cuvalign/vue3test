f0bb524de09a32a455de35f6dba5ed3dad323303
cuvalign <akrddr@163.com>
2021-11-21 14:54:03 +0800

    fixed：小尝试

---
+++ firstvue30/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{message}}</h2>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  setup () {
+    const message = ref('vue 3.0的欢迎信息！')
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h2 {
+  margin: 40px 0 0;
+  color: orangered;
+}
+</style>
